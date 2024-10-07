7f1b048220742e1994b4691e1f29f7070aa09e1e
felixmathew03 <felixmathewtt@gmail.com>
2024-10-07 09:12:15 +0530

    2

--- serverside/models/user.model.js
+++ serverside/models/user.model.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     email:{type:String},
     username:{type:String},
-    password:{type:String}
+    password:{type:String},
+    place:{type:String},
+    address:{type:String},
+    phone:{type:Number},
+    pincode:{type:Number}
 })
 export default mongoose.model.Users||mongoose.model("User",userSchema)
